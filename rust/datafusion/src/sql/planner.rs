157d2a1e112c3c2b3a68067a3c0cb15215d7a602
Morgan Cassels <morgan@urbanlogiq.com>
2020-02-18 16:51:30 -0800

    more debugging

--- rust/datafusion/src/sql/planner.rs
+++ rust/datafusion/src/sql/planner.rs
@@ -472,7 +472,7 @@ pub fn convert_data_type(sql: &SQLType) -> Result<DataType> {
         SQLType::Float(_) | SQLType::Real => Ok(DataType::Float64),
         SQLType::Double => Ok(DataType::Float64),
         SQLType::Char(_) | SQLType::Varchar(_) => Ok(DataType::Utf8),
-        SQLType::Timestamp => Ok(DataType::Timestamp(TimeUnit::Millisecond, None)),
+        SQLType::Timestamp => Ok(DataType::Timestamp(TimeUnit::Nanosecond, None)),
         other => Err(ExecutionError::NotImplemented(format!(
             "Unsupported SQL type {:?}",
             other
